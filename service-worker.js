fdb42620f55b66e850e259cf41df5395d0556338
Executive_Eagle <christophershelley257@gmail.com>
2026-09-17 22:02:38 -0500

    Refresh Northstar fault-isolated loader

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="northstar-shell-v4";
+const CACHE="northstar-shell-v5";
 const SHELL=["./","./index.html","./styles.css","./clinical.css","./northstar-enterprise.css","./favicon.svg","./manifest.webmanifest"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
