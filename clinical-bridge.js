a5ea1dd692deafc9495ec3f87e208914c72be6f8
Executive_Eagle <christophershelley257@gmail.com>
2026-09-19 15:51:26 -0500

    Refresh Staff ID and record lock build

--- clinical-bridge.js
+++ clinical-bridge.js
@@ -1,4 +1,4 @@
-const NORTHSTAR_BUILD = "20260919-1705";
+const NORTHSTAR_BUILD = "20260919-1720";
 const moduleUrl = path => `${path}?v=${NORTHSTAR_BUILD}`;
 window.NorthstarBuild = NORTHSTAR_BUILD;
 const failedModules = [];
